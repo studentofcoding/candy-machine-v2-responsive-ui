70a7866f8a21315c4f534a6e15cf57b61c44fe6a
Yonathan Evan C <yonathanevan@gmail.com>
2022-06-07 18:46:54 +0700

    Update mint button

--- src/MintButton.tsx
+++ src/MintButton.tsx
@@ -69,7 +69,7 @@ export const MintButton = ({
             variant="contained"
         >
             {!candyMachine ? (
-                "CONNECTING..."
+                "Connect your wallet..."
             ) : candyMachine?.state.isSoldOut || isSoldOut ? (
                 'SOLD OUT'
             ) : isActive ? (
